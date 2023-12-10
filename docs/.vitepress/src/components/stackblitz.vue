4b997beff0a619c81b3957c93b054496bcb20bf9
wzc520pyfm <69044080+wzc520pyfm@users.noreply.github.com>
2023-12-10 01:58:02 +0000

    feat(docs): support change dark theme

--- docs/.vitepress/src/components/stackblitz.vue
+++ docs/.vitepress/src/components/stackblitz.vue
@@ -1,28 +1,39 @@
 <script lang="ts" setup>
 import { ref, watchEffect } from 'vue'
+import { useData } from 'vitepress'
 import sdk from '@stackblitz/sdk';
+import type { VM } from '@stackblitz/sdk';
 
 defineOptions({ name: 'Stackblitz' })
 
-const world = ref('Vitepress')
+const { isDark } = useData()
+const welcome = ref('Welcome Vitest Interactive!')
 const containerRef = ref(null);
+const stackblitzVm = ref<VM | null>(null);
 
-watchEffect(() => {
+watchEffect(async () => {
   if (containerRef.value) {
-    sdk.embedGithubProject(
+    stackblitzVm.value = await sdk.embedGithubProject(
       containerRef.value, 
       'wzc520pyfm/vitest-interactive',
       {
         height: window.innerHeight - document.querySelector('.VPNav')?.clientHeight!,
         view: 'preview',
+        theme: isDark.value ? 'dark' : 'light',
         hideExplorer: true,
         hideNavigation: true,
       }
     )
   }
 })
+
+watchEffect(() => {
+  if (stackblitzVm.value) {
+    stackblitzVm.value.editor.setTheme(isDark.value ? 'dark' : 'light')
+  }
+})
 </script>
 
 <template>
-  <div ref="containerRef">{{ world }}</div>
+  <div ref="containerRef">{{ welcome }}</div>
 </template>
